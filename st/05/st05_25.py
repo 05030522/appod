7fbbf944f18dba715f082abcbf594060e03026dc
jin <132440453+05030522@users.noreply.github.com>
2025-05-26 02:47:21 +0900

    0525

---
+++ st/05/st05_25.py
@@ -0,0 +1,9 @@
+empty_list = []
+print(empty_list)       # 출력: []
+print(type(empty_list)) # 출력: <class 'list'>
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)          # 출력: [1, 2, 3, 4, 5]
+
+mixed_list = [1, "안녕", 3.14, True]
+print(mixed_list)       # 출력: [1, '안녕', 3.14, True]
